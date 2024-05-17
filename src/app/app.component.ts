fd495c5903560294b2278378092c1975045e0b60
Alok Verma <as1492@srmist.edu.in>
2024-05-18 00:17:02 +0530

    Hehehe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ToDo} from './todo';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'ToDo';
+  title = 'ToDo App';
+  name: string = "Alok"
+
+  todoValue: string = " ";
+  list: ToDo[] = [];
+
+  ngOnInit() {
+    this.todoValue = "";
+    this.list = [];
+  }
+
+  addItem() {
+    if(this.todoValue !== "") {
+      const newItem: ToDo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id: number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
+
+
---
+++ src/app/todo.ts
@@ -0,0 +1,5 @@
+export interface ToDo {
+    id: number;
+    value: string;
+    isDone: boolean;
+}
